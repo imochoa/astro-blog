65325f3a5bef505fb3624959ae086be0e360b76b
imochoa <18612100+imochoa@users.noreply.github.com>
2026-08-17 22:07:15 +0200

    added satteri config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, logHandlers } from "astro/config";
+import { satteri } from "@astrojs/markdown-satteri";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  logger: logHandlers.json(),
+  markdown: {
+    processor: satteri({
+      features: { directive: true, math: true, headingAttributes: true },
+    }),
+  },
+});
